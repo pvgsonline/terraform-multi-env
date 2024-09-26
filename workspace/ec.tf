1724c80bc18fb3f626633c4cfa01c0fe4eac1e9b
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-09-26 11:56:36 +0530

    terraform

--- workspace/ec.tf
+++ workspace/ec.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terraform"{
     ami = "ami-09c813fb71547fc4f"
-    instance_type = lookup(var.instance, terraform.workspace)
+    instance_type = lookup(var.instances, terraform.workspace)
     vpc_security_group_ids = ["sg-04ae891e9bad6b5b6"]
 
     tags = {

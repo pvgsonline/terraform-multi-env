37f5af22b8378db0ceda74f3c5541a65e4aa54b6
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-09-26 10:24:21 +0530

    terraform

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "pvgs-remote-state-dev"
+key    = "tfvars-demo-dev"
+region = "us-east-1"
+dynamodb_table = "pvgs-locking-dev"
---
+++ tfvars/variables.tf
@@ -0,0 +1,23 @@
+variable instances {
+  type        = map
+  
+}
+
+variable "zone_id" {
+    default = "Z0374240SJG94LFMUIHX"
+}
+
+variable "domain_name" {
+    default = "daws81s.online"
+}
+
+variable "common_tags" {
+  default = {
+  Project = "expense"
+  terraform = true
+}
+}
+
+variable "tags" {
+  type = map
+}
